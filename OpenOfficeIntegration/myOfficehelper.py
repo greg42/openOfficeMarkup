b49325a2187248d034fde6654fbc41c7824ad750
joernchen of Phenoelit <joernchen@phenoelit.de>
2014-10-07 10:11:44 +0200

    Change to socket instead of pipe
    OO communication via TCP socket, renderer terminates now gracefully

--- OpenOfficeIntegration/myOfficehelper.py
+++ OpenOfficeIntegration/myOfficehelper.py
@@ -22,47 +22,48 @@ class OpenOfficeInstance(object):
       if "UNO_PATH" in os.environ:
          office = os.environ["UNO_PATH"]
       else:
-         if platform.startswith("win"): 
+         if platform.startswith("win"):
             # XXX
             office = ""
          else:
             # Lets hope that works..
             office = '/usr/bin'
       office = os.path.join(office, "soffice")
-      if platform.startswith("win"): 
+      if platform.startswith("win"):
           office += ".exe"
-	
-      self._pipeName = "uno_template_foobar_23_42"
-       
+
+      self._pidfile = "/tmp/recurity_renderer_OOinstance" #XXX Windows compat needed
       xLocalContext = uno.getComponentContext()
       self._resolver = xLocalContext.ServiceManager.createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver", xLocalContext)
-      self._connect = "uno:pipe,name=" + self._pipeName + ";urp;StarOffice.ComponentContext"
+      self._socket = "host=127.0.0.1,port=2002,tcpNoDelay=1"
 
-      args = ["-invisible", "-nologo", "-nodefault", "-norestore", "--nofirststartwizard"]
+      args = ["--invisible", "--nologo", "--nodefault", "--norestore", "--nofirststartwizard"]
       if headless:
-         args.append("-headless")
+         args.append("--headless")
 
       if platform.startswith("win"):
-         cmdArray = ['"' + office + '"'] 
+         cmdArray = ['"' + office + '"']
       else:
          cmdArray = [office]
-      cmdArray += args + ["-accept=pipe,name=%s;urp;" % self._pipeName]
-      self.pid = os.spawnv(os.P_NOWAIT, office, cmdArray)
+      cmdArray += args + ["--accept=socket,%s;urp;" % self._socket]
+      if( not os.path.isfile(self._pidfile)):
+         self.pid = os.spawnv(os.P_NOWAIT, office, cmdArray)
+         f = open(self._pidfile,"w")
+         f.write(str(self.pid))
 
    def close(self):
-      try:
-         os.killpg(os.getpgid(self.pid), signal.SIGTERM)
-      except:
-         pass
+     if(self.pid):
+        os.kill(self.pid, signal.SIGTERM)
+        os.remove(self._pidfile)
 
    def getContext(self):
       context = None
       for _ in range(20):
           try:
-              context = self._resolver.resolve(self._connect)
+              context = self._resolver.resolve("uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
               break
           except NoConnectException:
-              sleep(0.5) 
+              sleep(0.5)
 
       if not context:
          raise RuntimeError("Cannot connect to soffice.", None)
